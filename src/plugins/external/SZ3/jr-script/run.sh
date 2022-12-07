0ff74cb9eed01cb6a1bd3490fdda769bf2700711
John Ravi <jjravi@ncsu.edu>
2022-12-07 03:41:25 -0500

    update

--- src/plugins/external/SZ3/jr-script/run.sh
+++ src/plugins/external/SZ3/jr-script/run.sh
@@ -10,12 +10,12 @@ do
   echo "running $i times"
   timeout 10 ./cpu_mon > cpu_stats$i.csv &
   sleep 1
-  mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/temperature.f32 -3 512 512 512  > app_stats$i.txt
+  # mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/temperature.f32 -3 512 512 512  > app_stats$i.txt
   # mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/QRAINf48.log10.bin.f32 -3 100 500 500 > app_stats$i.txt
   # mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/QCLOUDf48.log10.bin.f32 -3 100 500 500  > app_stats$i.txt
   # mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/QVAPORf48.bin.f32 -3 100 500 500  > app_stats$i.txt
-  # mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/einspline_115_69_69_288.f32 -4 115 69 69 288 > app_stats$i.txt
-  while pgrep -lf "sz3" > /dev/null; do
+  mpirun -np $i ./sz3 -f -M PSNR -S 80 -z -i /dev/shm/einspline_115_69_69_288.f32 -4 115 69 69 288 > app_stats$i.txt
+  while pgrep -lf "^sz3$" > /dev/null; do
     sleep 1
   done
   while pgrep -lf "cpu_mon" > /dev/null; do
